166c526639b0a6bfef54d018529e3cd06d6f9c9b
wgevaert <woutgevaert@gmail.com>
2019-12-03 23:11:15 +0100

    Haal debug-output weg && fix edge-case
    Alhoewel die edge-case volgens mij niet zou voorkomen.

--- 2019/3.2.cpp
+++ 2019/3.2.cpp
@@ -7,9 +7,9 @@ int main() {
         field[i]=new long unsigned[2*oy];
     }
 
-    int px=ox, py=oy, step, x_min=ox,x_max=ox,y_min=oy,y_max=oy;
+    int px=ox, py=oy, step;
     long long unsigned min_score=2*ox*ox;
-    long unsigned dist=0, score;std::cout<<min_score;
+    long unsigned dist=0, score;
     char direction, dummy;
     do {
         direction = std::cin.get();std::cin>>step; // get input.
@@ -27,18 +27,13 @@ for(int j=0;j<2*ox;j++)delete[] field[j];
 return 1;
             }
             dist++;
-            field[px][py] = dist;
-        }
-        switch(direction) {
-            case 'U':if(y_max<py)y_max=py;break;
-            case 'D':if(y_min>py)y_min=py;break;
-            case 'R':if(x_max<px)x_max=px;break;
-            case 'L':if(x_min>px)x_min=px;break;
+            if (!field[px][py]) {
+                field[px][py] = dist;
+            }
         }
     } while((dummy = std::cin.get()) == ',');
-std::cout<<"First cable layout:"<<std::endl;for (int y=y_max;y>=y_min;y--){std::cout<<y-oy<<' ';for(int x=x_min;x<=x_max;x++)std::cout<<(field[x][y] ? 'X' : ((x==ox&&y==oy)?'O':'.'));std::cout<<std::endl;}
 
-    px=ox;py=oy;dist=0;std::cout<<"Found crossings:"<<std::endl;
+    px=ox;py=oy;dist=0;
     do {
         direction = std::cin.get();std::cin>>step; // get input.
         for(int i=1;i<=step;i++) {
@@ -55,9 +50,9 @@ for(int j=0;j<2*ox;j++)delete[] field[j];
 return 1;
             }
             dist++;
-            if (field[px][py]) {std::cout<<px-ox<<' '<<py-oy<<std::endl;if((score = field[px][py]+dist) < min_score) {
+            if (field[px][py] && (score = field[px][py]+dist) < min_score) {
                 min_score = score;
-            }}
+            }
         }
     } while(std::cin.get() == ',');
     std::cout<<"Minimal score found: "<<min_score<<std::endl;
