cc4ae8340d8372842f47c0be72dd019fa0faf7c6
wgevaert <woutgevaert@gmail.com>
2019-12-04 10:52:39 +0100

    Fix mijn eerste poging

--- 2019/4.cpp
+++ 2019/4.cpp
@@ -9,6 +9,14 @@ long long supersum(int value, int depth) {
     return result;
 }
 
-int main() {//Unfortunately, does not work correctly...
-    std::cout<<supersum(8,5)-supersum(4,5)+supersum(4,3)+supersum(2,3)+supersum(2,2)<<std::endl;return 0;
+int main() {
+    // Input is 178416-676461
+    // returns 1716-84+35+6+5-28 = 1650
+    std::cout<<supersum(8,5)-supersum(4,5) // All increasing sequences >= 2e+5, < 6e+5
+               +supersum(4,3) // All increasing sequences >= 6.6e+5, < 6.7e+5
+               +supersum(2,4) // All increasing sequences >= 1.8e+5, < 2e+5
+               +supersum(2,3) // All increasing sequences >= 1.78e+5, <1.8e+5
+               -supersum(3,5) // All strictly-increasing sequences in range
+             <<std::endl;
+    return 0;
 }
