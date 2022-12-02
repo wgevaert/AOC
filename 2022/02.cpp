d4bce24a788528bfc7c06c2ee5af8f891063e39f
Wout Gevaert <wout@wikibase.nl>
2022-12-02 17:22:05 +0100

    Day 2, luckily easy

---
+++ 2022/02.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <string>/*
+#include <vector>
+#include <unordered_map>*/
+
+// Because I'm too lazy to type
+typedef uint64_t ull_t;
+typedef uint32_t u_t;
+typedef int64_t ll_t;
+
+unsigned verb_lvl = 0;
+
+void read_or_die(std::string pattern, std::istream& input) {
+    for (auto a:pattern) {
+        if (a != input.get()) {
+            std::string parsed_pattern = "";
+            for (auto b:pattern) {
+                if (b < ' ' || b > 126)
+                    parsed_pattern += '?';
+                else
+                    parsed_pattern += b;
+            }
+            std::cerr<<'\''<<parsed_pattern<<'\''<<" expected"<<std::endl;
+            exit(1);
+        }
+    }
+}
+
+int real_main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr<<"Usage: "<<argv[0]<<" [-v {verbosity_level}] {input_file}"<<std::endl;
+        exit(1);
+    }
+    verb_lvl = argc > 3 && argv[1][0] == '-' && argv[1][1] == 'v' ? std::stoul(argv[2]) : 0;
+
+    std::ifstream input(argv[argc - 1]);
+
+    if (!input.good()) {
+        std::cerr<<"Input file "<<argv[argc - 1]<<" did not open correctly"<<std::endl;
+        exit(1);
+    }
+
+    if (verb_lvl > 0) {
+        std::cout<<"Running in verbosity mode "<<verb_lvl<<std::endl;
+    }
+
+    char a,b;
+    u_t s=0,s2=0;
+    do {
+        a=input.get();read_or_die(" ",input);b=input.get();read_or_die("\n",input);
+        s += b-'X'+3*((4+(b-'X')-(a-'A'))%3)+1;
+        s2 += (2+b-'X'+a-'A')%3+3*(b-'X')+1;
+    }while(input.peek()!='\n'&&!input.eof());
+    input.close();
+
+    std::cout<<s<<' '<<s2<<std::endl;
+    return 0;
+}
+
+int main (int argc, char** argv) {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    int result = real_main(argc,argv);
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    std::cout<<"Duration: "<<std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count()<<std::endl;
+    return result;
+}
