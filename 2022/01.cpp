bd69a16cb4f4020638a8c81c5959a01fce7db995
Wout Gevaert <wout@wikibase.nl>
2022-12-02 01:28:04 +0100

    Day 1 2022

---
+++ 2022/01.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <string>
+
+// Because I'm too lazy to type
+typedef uint64_t ull_t;
+typedef uint32_t u_t;
+typedef int64_t ll_t;
+
+unsigned verb_lvl = 0;
+
+void read_or_die(std::string pattern, std::istream& input) {
+    for (auto a:pattern) {
+        if (a != input.get()) {
+            std::string parsed_pattern = "";
+            for (auto b:pattern) {
+                if (b < ' ' || b > 126)
+                    parsed_pattern += '?';
+                else
+                    parsed_pattern += b;
+            }
+            std::cerr<<'\''<<parsed_pattern<<'\''<<" expected"<<std::endl;
+            exit(1);
+        }
+    }
+}
+
+int real_main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr<<"Usage: "<<argv[0]<<" [-v {verbosity_level}] {input_file}"<<std::endl;
+        exit(1);
+    }
+    verb_lvl = argc > 3 && argv[1][0] == '-' && argv[1][1] == 'v' ? std::stoul(argv[2]) : 0;
+
+    std::ifstream input(argv[argc - 1]);
+
+    if (!input.good()) {
+        std::cerr<<"Input file "<<argv[argc - 1]<<" did not open correctly"<<std::endl;
+        exit(1);
+    }
+
+    if (verb_lvl > 0) {
+        std::cout<<"Running in verbosity mode "<<verb_lvl<<std::endl;
+    }
+
+    unsigned long cur=0,in,max=0,max1=0,max2=0;
+    while(!input.eof()) {
+        while(std::isdigit(input.peek()) && !input.eof()) {
+            input>>in;
+            cur += in;
+            read_or_die("\n",input);
+        }
+        if ( cur >= max ) {
+            max2 = max1;
+            max1 = max;
+            max = cur;
+        } else if ( cur >= max1 ) {
+            max2 = max1;
+            max1 = cur;
+        } else if ( cur > max2 ) {
+            max2 = cur;
+        }
+        cur = 0;
+        if (!input.eof())
+            read_or_die("\n",input);
+    }
+    std::cout<<max<<' '<<(max+max1+max2)<<"\n";
+
+    // do things with input.
+
+    input.close();
+
+    // do things without input
+    return 0;
+}
+
+int main (int argc, char** argv) {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    int result = real_main(argc,argv);
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    std::cout<<"Duration: "<<std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count()<<std::endl;
+    return result;
+}
