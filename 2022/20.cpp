3d3ac95e39071501ca4f9e0f99d261613ed96571
Wout Gevaert <wout@wikibase.nl>
2022-12-20 10:31:16 +0100

    A linked list in a vector, best abomination I've ever created!

--- 2022/20.cpp
+++ 2022/20.cpp
@@ -28,16 +28,17 @@ void read_or_die(std::string pattern, std::istream& input) {
     }
 }
 
-const bool part2=false;
+const bool part2=true;
 
 class dll_ele {
 public:
     ll_t val;
     dll_ele *front, *back;
-    dll_ele(ll_t v):val(v){front=back=this;}
-    void add_to_front(ll_t v) {
+    dll_ele(ll_t v):val(v){}
+    dll_ele(){}
+    void add_to_front(dll_ele* e) {
         dll_ele* old_front=front;
-        old_front->back = front = new dll_ele(v);
+        old_front->back = front = e;
         front->back = this;
         front->front = old_front;
     }
@@ -70,47 +71,51 @@ int real_main(int argc, char** argv) {
     }
 
     const ll_t p2const = 811589153;
-    ll_t v, pos0;
+    ll_t v, pos0, size=1;
     input>>v;
     if (part2)v*=p2const;
-    dll_ele* mylist= new dll_ele(v);
-    std::vector<dll_ele*> ref={mylist};
+    dll_ele ref[5000];
+    ref[0] = dll_ele(v);
+    ref[0].back=ref[0].front=&ref[0];
+
+    dll_ele* mylist= &ref[0];
     read_or_die("\n",input);
     while (input.peek()=='-'||std::isdigit(input.peek())) {
         input>>v;
         if (part2)v*=p2const;
-        if (v==0)pos0=ref.size();
-        mylist->add_to_front(v);
+        if (v==0)pos0=size;
+        ref[size]=dll_ele(v);
+        mylist->add_to_front(&ref[size]);
+        size++;
         mylist = mylist->front;
-        ref.emplace_back(mylist);
         read_or_die("\n",input);
     }
     input.close();
 
     if(verb_lvl>1){auto a=mylist;do{std::cout<<a->val<<' ';a=a->front;}while(a!=mylist);std::cout<<std::endl;}
     for (int i=0;i<1+9*part2;i++)
-    for (auto a:ref) {
-        ll_t A=a->val;
+    for (u_t j=0;j<size;j++) {
+        ll_t A=ref[j].val;
         if (A<=0) {
             // There's a -1 because the element we are moving is not counted.
-            A = -1*((-1*A)%static_cast<ll_t>(ref.size()-1));
-            a->loosen();
-            for (auto b=0;b<-1*A;b++)a->back=a->back->back;
-            a->front = a->back->front;
-            a->fasten();
+            A = -1*((-1*A)%static_cast<ll_t>(size-1));
+            ref[j].loosen();
+            for (auto b=0;b<-1*A;b++)ref[j].back=ref[j].back->back;
+            ref[j].front = ref[j].back->front;
+            ref[j].fasten();
         } else {
             // There's a -1 because the element we are moving is not counted.
-            A = A%(ref.size()-1);
-            a->loosen();
-            for (auto b=0;b<A;b++)a->front=a->front->front;
-            a->back=a->front->back;
-            a->fasten();
+            A = A%(size-1);
+            ref[j].loosen();
+            for (auto b=0;b<A;b++)ref[j].front=ref[j].front->front;
+            ref[j].back=ref[j].front->back;
+            ref[j].fasten();
         }
-        if(verb_lvl>1)std::cout<<"Moved "<<a->val<<" between "<<a->back->val<<" and "<<a->front->val<<std::endl;
+        if(verb_lvl>1)std::cout<<"Moved "<<ref[j].val<<" between "<<ref[j].back->val<<" and "<<ref[j].front->val<<std::endl;
     }
 
     ll_t ans=0;
-    mylist=ref[pos0];
+    mylist=&ref[pos0];
     for(auto b=0;b<1000;b++)mylist=mylist->front;
     if(verb_lvl)std::cout<<"1000 has "<<mylist->val<<std::endl;
     ans +=mylist->val;
@@ -122,7 +127,6 @@ int real_main(int argc, char** argv) {
     ans +=mylist->val;
 
     std::cout<<ans<<std::endl;
-    // do things without input
     return 0;
 }
 
